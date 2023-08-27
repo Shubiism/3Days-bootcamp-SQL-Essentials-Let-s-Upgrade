5b18ef2f6fd7d6e65189148be0aaf6bd85bf5451
Shubiism <97299518+Shubiism@users.noreply.github.com>
2023-08-28 02:38:42 +0530

    Task2.sql

--- Task2.sql
+++ Task2.sql
@@ -4,3 +4,12 @@ CREATE TABLE Books (
     Author VARCHAR(255) NOT NULL,
     `Published Year` INT NOT NULL
 );
+
+INSERT INTO Books (Title, Author, `Published Year`)
+VALUES
+    ('Harry Potter', 'J.K. Rowling', 1997),
+    ('The Great Gatsby', 'F. Scott Fitzgerald', 1925),
+    ('To Kill a Mockingbird', 'Harper Lee', 1960);
+
+SELECT Title, Author, `Published Year`
+FROM Books;
